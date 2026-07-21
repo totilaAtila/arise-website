1820278f44c7283c522559706c4c8c9a709be36e
totila6 <totila6@gmail.com>
2026-07-21 13:43:17 +0100

    Update Google Play internal URL in config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const appConfig = {
   githubUrl: 'https://github.com/totilaAtila/Arise',
   releasesUrl: 'https://github.com/totilaAtila/Arise/releases',
   googlePlayUrl: 'https://play.google.com/store/apps/details?id=com.arise.alarm',
-  googlePlayInternalUrl: 'https://play.google.com/apps/internaltest/4701487278692854374',
+    googlePlayInternalUrl: 'https://play.google.com/apps/testing/com.arise.alarm',
   version: '1.0',
   lastUpdated: 'April 2026',
 };
